b86ddf20c4ff0e13914918752ab3d55f1cec5b52
K. Cromer <70463509+theCromers@users.noreply.github.com>
2021-08-18 17:02:31 -0400

    third commit
    should be correct now

---
+++ MyLife_App/MyLife_App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyLife_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Kayla Cromer."); //code written 6.8.2021 @01:03 EDT
+            Console.WriteLine("");
+            Console.WriteLine("I was born in Atlanta, Georgia."); 
+            Console.WriteLine("");
+            Console.WriteLine("My favorite video game right now is Warframe."); //highly recommended. I've sunk about 145 hours into it. 
+            Console.WriteLine("");
+            Console.WriteLine("I usually play games on a PC."); //converted from console. I haven't looked back.
+        }
+    }
+}
